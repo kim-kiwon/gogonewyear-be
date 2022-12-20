cd374ee11066882729b5ed59372dff7bc190a855
kim-kiwon <kimkiwonn@gmail.com>
2022-12-20 23:41:16 +0900

    feat: 기존 enum 반환시 객체명만 반환되어, errorCode 응답에서 객체 전체 반환하도록 Jackson 설정

--- core/src/main/java/com/gogonew/api/core/exception/ErrorCode.java
+++ core/src/main/java/com/gogonew/api/core/exception/ErrorCode.java
@@ -1,8 +1,12 @@
 package com.gogonew.api.core.exception;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 import lombok.Getter;
 
+
 @Getter
+@JsonFormat(shape = Shape.OBJECT) // Json 응닶으로 Enum 값만이 아닌 객체 전체 반환하도록
 public enum ErrorCode {
     SERVER_ERROR(500, "C000", "서버 오류가 발생하였습니다."),
     INVALID_INPUT_VALUE(400, "C001", "잘못된 입력입니다.");
