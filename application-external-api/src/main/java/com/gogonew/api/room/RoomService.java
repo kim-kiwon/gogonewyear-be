47d2afb750da6878e442912b0793577dbe2949d2
kim-kiwon <kimkiwonn@gmail.com>
2022-12-31 13:22:30 +0900

    feat: RoomService에 Transactional AOP 추가

--- application-external-api/src/main/java/com/gogonew/api/room/RoomService.java
+++ application-external-api/src/main/java/com/gogonew/api/room/RoomService.java
@@ -8,21 +8,25 @@ import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class RoomService {
     private final RoomRepository roomRepository;
 
+    @Transactional(readOnly = true)
     public List<RoomDto.Response> getAllRoom() {
         return RoomDto.Response.ofList(roomRepository.findAll());
     }
 
+    @Transactional(readOnly = true)
     public RoomDto.Response getRoom(UUID roomId) {
         Room room = roomRepository.findById(roomId).orElseThrow(() -> new ApiException(ErrorCode.NO_DATA));
         return RoomDto.Response.of(room);
     }
 
+    @Transactional
     public RoomDto.Response addRoom(RoomDto.Create request) {
         Room room = request.toEntity();
         roomRepository.save(room);
